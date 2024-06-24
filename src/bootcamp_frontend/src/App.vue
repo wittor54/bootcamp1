24c1ab9957b06c877c2a30ac495483e2e1990a49
wittor54 <xdwittor@gmail.com>
2024-06-24 21:02:22 +0200

    fix

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    Siema!!!
+    <br>
+    {{ licznik }}
+    <button @click="podbij">podbij</button>
+  </main>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      licznik: 0
+    }
+  },
+  methods:{
+    podbij(){
+      this.licznik++
+    }
+  }
+}
+</script>
